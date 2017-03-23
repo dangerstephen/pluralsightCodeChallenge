d3a8c336819512d156d5f96e55d1128f8e237871
dangerstephen <dangersteve1@gmail.com>
2017-03-22 17:43:53 -0700

    Need to work on the Range Error, I plan to add a try and catch method

--- app.js
+++ app.js
@@ -1,17 +1,64 @@
-var input = ["KittenService: CamelCaser","CamelCaser: "];
+// var input = ["KittenService: CamelCaser","CamelCaser: "];
+// This should return "CamelCaser, KittenService"
+
+// var input =["KittenService: ", "Leetmeme: Cyberportal", "Cyberportal: Ice", "CamelCaser: KittenService", "Fraudstream: Leetmeme", "Ice: "];
+// This should return "KittenService, Ice, Cyberportal, Leetmeme, CamelCaser, Fraudstream"
+
+var input =["KittenService: ", "Leetmeme: Cyberportal", "Cyberportal: Ice", "CamelCaser: KittenService", "Fraudstream: ", "Ice: Leetmeme"];
+// This should be rejected because of the cycle
+
 var output = [];
-output.push(input);
 $('.content').append('<div class="card"><p>[ '+ input +' ]</p></div>');
 
 
-function Order(input) {
+function pkgOrder(input) {
   if (input.length === 0){
     $('.content').append('<div class="card"><p>Sorry the information provided is invalid, please try again</p></div>');
     console.log("Sorry the information provided is invalid, please try again");
   } else {
+    for (var i = 0; i < input.length; i++) {
+            pkgInstall(input[i].trim());
+        }
     $('.content').append('<div class="card"><p>'+ output +'</p></div>');
     console.log(input);
   }
 }
 
-Order(input);
+function pkgSearch(pkg) {
+    for (var i = 0; i < input.length; i++) {
+        var pkgParas = input[i].trim().split(':');
+
+        if (pkgParas[0] != '' && pkgParas[0].indexOf(pkg) > -1) {
+            return input[i];
+        }
+    }
+    return null;
+}
+
+function pkgCheck(pkg) {
+    return output.indexOf(pkg) > -1;
+}
+
+function pkgInstall(pkg) {
+    var pkgParas = pkg.split(':');
+
+    if (pkgParas.length > 1 && pkgParas[1].trim() != '') {
+        var dependency = pkgParas[1].trim();
+        var pkgSearchResult = pkgSearch(dependency);
+
+        if (pkgSearchResult != null) {
+            pkgInstall(pkgSearchResult);
+        } else {
+            output.push(dependency);
+        }
+    }
+    if (pkgCheck(pkgParas[0].trim())) {
+        return;
+    }
+
+    else  {
+        output.push(pkgParas[0].trim());
+    }
+}
+
+pkgOrder(input);
