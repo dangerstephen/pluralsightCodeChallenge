7b75b45a22bb97358142ee49ea18fa9a32639811
dangerstephen <dangersteve1@gmail.com>
2017-03-22 19:57:32 -0700

    Adding in files that will be include the application questions

--- app.js
+++ app.js
@@ -7,20 +7,20 @@ var input =["KittenService: ", "Leetmeme: Cyberportal", "Cyberportal: Ice", "Cam
 // var input =["KittenService: ", "Leetmeme: Cyberportal", "Cyberportal: Ice", "CamelCaser: KittenService", "Fraudstream: ", "Ice: Leetmeme"];
   // This should be rejected because of the cycle
 
-  $('.content').append('<div class="card"><h1>Orignial Input:</h1><p>[ '+ input +' ]</p></div>');
+  $('.appending').append('<div class="card"><h1 class="title">Orignial Input:</h1><p>[ '+ input +' ]</p></div>');
 
 var output = [];
 
 
 function pkgOrder(input) {
   if (input.length === 0 || !input){
-    $('.content').append('<div class="card"><p>Sorry the information provided is invalid, please try again</p></div>');
+    $('.appending').append('<div class="card"><p>Sorry the information provided is invalid, please try again</p></div>');
     console.log("Sorry the information provided is invalid, please try again");
   } else {
     for (var i = 0; i < input.length; i++) {
             pkgInstall(input[i].trim());
         }
-        $('.content').append('<div class="card"><h1>Output:</h1><p>'+ output +'</p></div>');
+        $('.appending').append('<div class="card"><h1 class="title">Output:</h1><p>'+ output +'</p></div>');
         console.log(input);
   }
 }
@@ -60,11 +60,11 @@ function pkgInstall(pkg) {
     else {
         output.push(pkgParas[0].trim());
         if(output.length != input.length){
-          $('.content').append('<div class="card errors"><h1>Output:</h1><p>Sorry the information provided is invalid, please try again</p></div>');
+          $('.appending').append('<div class="card errors"><h1 class="title">Output:</h1><p>Sorry the information provided is invalid, please try again</p></div>');
         } else if (output.length === input.length){
           $('.errors').addClass('hidden');
         } else {
-          $('.content').append('<div class="card errors"><p>This is awkward. But an unecptected error occured</p></div>');
+          $('.appending').append('<div class="card errors"><p>This is awkward. But an unecptected error occured</p></div>');
         }
 
     }
